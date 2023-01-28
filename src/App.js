2549dfa0f396e6728c9a9e3ffd77d3f65a377bc3
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-28 11:07:49 -0600

    Added params for product details

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Route } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
-import Products from './pages/Product';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
         <Route path='/products'>
           <Products />
         </Route>
+        <Route path='/product-detail/:productId'>
+          <ProductDetail />
+        </Route>
       </main>
     </div>
   );
@@ -24,3 +28,4 @@ export default App;
 
 // our-domain.com/welcome => Welcome Component
 // our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book
